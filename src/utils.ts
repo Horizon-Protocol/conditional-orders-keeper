fc80f83527ffbccfaec8ec873a2922e2c0380863
rout-horizon <rout.c@horizonprotocol.com>
2024-09-18 20:02:09 +0530

    removed WS

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();  // Load environment variables from .env file 
 
 import { ethers } from 'ethers';
-import { WebSocketProvider } from './customWebsocket';
+// import { WebSocketProvider } from './customWebsocket';
 import { ABI } from './constants';
 
 import {
@@ -16,13 +16,13 @@ import {
 
 
 // Ethers Provider and Signer
-const wsprovider1 = new WebSocketProvider(wss);
+// const wsprovider1 = new WebSocketProvider(wss);
 export const rpcprovider = new ethers.providers.JsonRpcProvider(rpc);
 const wallet = new ethers.Wallet(signerPrivateKey);
 export const signer = wallet.connect(rpcprovider);
 
 export const createContracts = () => {
-    const eventsContract = new ethers.Contract(eventsContractAddress, ABI, wsprovider1);
+    const eventsContract = new ethers.Contract(eventsContractAddress, ABI, rpcprovider);
     const multicall = new ethers.Contract(multicallAddress, ABI, rpcprovider);
     const accountContract = new ethers.Contract(accountContractAddress, ABI, rpcprovider);
 
